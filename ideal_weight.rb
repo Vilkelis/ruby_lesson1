5f37dead054fdbd320abf396eabc5923d9bfcce5
Stepan Golovanov <vilkelis@yandex.ru>
2019-11-20 15:39:04 +0500

    fixes according robocop recomendations

--- ideal_weight.rb
+++ ideal_weight.rb
@@ -1,19 +1,19 @@
-puts "+============================+"
-puts "| Вычисление идеального веса |"
-puts "+============================+"
+puts '+============================+'
+puts '| Вычисление идеального веса |'
+puts '+============================+'
 
-puts "Ваше имя:"
+puts 'Ваше имя:'
 name = gets.chomp
 
-puts "Ваш рост (см):"
+puts 'Ваш рост (см):'
 height = gets.chomp.to_i
 
 ideal_weight = height - 110
 
-if ideal_weight < 0
-  message = "Ваш вес оптимален!"
-else
-  message = "Ваш идеальный вес #{ideal_weight} кг"
-end
+message = if ideal_weight < 0
+            'Ваш вес оптимален!'
+          else
+            "Ваш идеальный вес #{ideal_weight} кг"
+          end
 
 puts "Уважаемый #{name}! #{message}"
--- quadratic_equation.rb
+++ quadratic_equation.rb
@@ -1,14 +1,14 @@
-puts "+=================================+"
-puts "|       Квадратное уравнение      |"
-puts "+=================================+"
+puts '+=================================+'
+puts '|       Квадратное уравнение      |'
+puts '+=================================+'
 
-puts "Коэфициент a:"
+puts 'Коэфициент a:'
 a = gets.chomp.to_f
 
-puts "Коэфициент b:"
+puts 'Коэфициент b:'
 b = gets.chomp.to_f
 
-puts "Коэфициент c:"
+puts 'Коэфициент c:'
 c = gets.chomp.to_f
 
 puts "Решение уравнения #{a}*x^2 + #{b}*x + #{c} = 0"
@@ -21,15 +21,15 @@ if discriminant < 0
 else
   d_sqrt = Math.sqrt(discriminant)
 
-  x1 =  (-b + d_sqrt)/(2.0 * a)
+  x1 = (-b + d_sqrt) / (2.0 * a)
 
   if d_sqrt > 0
-    x2 =  (-b - d_sqrt)/(2.0 * a)
+    x2 = (-b - d_sqrt) / (2.0 * a)
 
     puts "  x1 = #{x1}"
     puts "  x2 = #{x2}"
   else
-    puts "  один корень у уравнения"
+    puts '  один корень у уравнения'
     puts "  x1 = #{x1}"
   end
 end
